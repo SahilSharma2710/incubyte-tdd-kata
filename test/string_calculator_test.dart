ed1fc697d645b264a774fb3d3b22426f1e17280f
sahilsharma-stage <sahil.sharma@stage.in>
2025-08-29 16:52:46 +0530

    Project SetUp & test for empty string and minimal implementation

---
+++ test/string_calculator_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import '../lib/string_calculator.dart';
+
+void main() {
+  group('StringCalculator', () {
+    late StringCalculator calculator;
+
+    setUp(() {
+      calculator = StringCalculator();
+    });
+
+    test('should return 0 for empty string', () {
+      expect(calculator.add(''), equals(0));
+    });
+  });
+}
