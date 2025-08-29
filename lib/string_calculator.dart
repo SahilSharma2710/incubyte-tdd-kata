c573280a390fede571d3b7a80d9219ed3ffbd94f
sahilsharma-stage <sahil.sharma@stage.in>
2025-08-29 17:05:35 +0530

    Added support for custom delimiters

--- lib/string_calculator.dart
+++ lib/string_calculator.dart
@@ -4,12 +4,28 @@ class StringCalculator {
       return 0;
     }
     
-    // Replace newlines with commas, then split by comma and sum all numbers
-    String normalizedNumbers = numbers.replaceAll('\n', ',');
+    String delimiter = ',';
+    String numbersToProcess = numbers;
+    
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+      int delimiterEndIndex = numbers.indexOf('\n');
+      delimiter = numbers.substring(2, delimiterEndIndex);
+      numbersToProcess = numbers.substring(delimiterEndIndex + 1);
+    }
+    
+    // Replace newlines with commas, then replace custom delimiter with commas
+    String normalizedNumbers = numbersToProcess.replaceAll('\n', ',');
+    if (delimiter != ',') {
+      normalizedNumbers = normalizedNumbers.replaceAll(delimiter, ',');
+    }
+    
     List<String> numberList = normalizedNumbers.split(',');
     int sum = 0;
     for (String number in numberList) {
-      sum += int.parse(number.trim());
+      if (number.isNotEmpty) {
+        sum += int.parse(number.trim());
+      }
     }
     return sum;
   }
